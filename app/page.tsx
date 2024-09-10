4aa803936fc21a88724f9ded20a8bfdadbb85c46
nitin <nitinrana01125532553@gmail.com>
2024-09-10 12:45:48 +0530

    added signIn and signUp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
+    // ****************************************************************************************************************
+    // This is the root folder, put the code for homepage here:
+
+    // TASK:
+    // keep a login button which is a link to /sign-in
+    // if the user is signed in (can check that by getting {userId} from auth() in clerk) display the upload button to upload pdf files
     <div>
       <p>Hello</p>
     </div>
