6bd51384d6f5898e611736cbfd7d3d7f2f5c6eac
nitin <nitinrana01125532553@gmail.com>
2024-09-20 13:22:45 +0530

    fixed upload

--- app/api/uploadFile/route.ts
+++ app/api/uploadFile/route.ts
@@ -3,15 +3,19 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
-  const filename = searchParams.get("filename") || "";
+  const filename: string = searchParams.get("filename") || ""; // get name from the URL
+  const fileKey: string = Date.now().toString() + filename.replace(" ", "-"); // generate a unique key for the file
 
-  // ⚠️ The below code is for App Router Route Handlers only
   if (filename && request.body) {
-    const blob = await put(filename, request.body, {
+    // upload the file with fileKey
+    const blob = await put(fileKey, request.body, {
       access: "public",
     });
 
-    return NextResponse.json(blob);
+    return NextResponse.json(
+      { fileKey, filename }, // return the fileKey and filename
+      { status: 200, headers: { "content-type": "application/json" } }
+    );
   } else {
     return NextResponse.json({ error: "No filename found" }, { status: 400 });
   }
